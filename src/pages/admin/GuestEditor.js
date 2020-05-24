7bd1fb94bd33a19206befcd2091387502e0f2392
Hareendra <hareendras@gmail.com>
2020-05-24 18:29:50 +0530

    minor

--- src/pages/admin/GuestEditor.js
+++ src/pages/admin/GuestEditor.js
@@ -14,11 +14,11 @@ const GuestEditor = ({ showGuestEditor, setShowGuestEditor, imgData }) => {
   };
 
   return (
-    <Modal open={showGuestEditor} closeIcon onClose={closeMe} size="big">
+    <Modal open={showGuestEditor} closeIcon onClose={closeMe} size="large">
       <Header icon="id card" content="ID Image" />
 
       <Modal.Content image>
-        <Image size="large" src={imgData} />{" "}
+        <Image  src={imgData} />{" "}
         <Modal.Description>
           <Container>
             <Form>
@@ -26,6 +26,10 @@ const GuestEditor = ({ showGuestEditor, setShowGuestEditor, imgData }) => {
                 <label>Guest Name</label>
 
                 <input placeholder="Guest Name" />
+              </Form.Field>
+              <Form.Field>
+                <label>NIC / Passport</label>
+                <input placeholder="NIC / PAssport" />
               </Form.Field>
               <Form.Field>
                 <label>Adress</label>
