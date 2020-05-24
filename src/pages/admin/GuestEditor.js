c6c61af5932749c1b91e91e611824654fe82b744
Hareendra Seneviratne <hareendras@gmail.com>
2020-05-24 16:37:47 +0530

    Guest editor

---
+++ src/pages/admin/GuestEditor.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  Header,
+  Modal,
+  Image,
+  Form,
+  Button,
+  Container,
+} from "semantic-ui-react";
+
+const GuestEditor = ({ showGuestEditor, setShowGuestEditor, imgData }) => {
+  const closeMe = () => {
+    setShowGuestEditor(false);
+  };
+
+  return (
+    <Modal open={showGuestEditor} closeIcon onClose={closeMe} size="big">
+      <Header icon="id card" content="ID Image" />
+
+      <Modal.Content image>
+        <Image size="large" src={imgData} />{" "}
+        <Modal.Description>
+          <Container>
+            <Form>
+              <Form.Field>
+                <label>Guest Name</label>
+
+                <input placeholder="Guest Name" />
+              </Form.Field>
+              <Form.Field>
+                <label>Adress</label>
+                <input placeholder="Adress" />
+              </Form.Field>
+              <Form.Field>
+                <label>E-mail</label>
+                <input placeholder="E-mail" />
+              </Form.Field>
+              <Form.Field>
+                <label>Phone</label>
+                <input placeholder="Phone" />
+              </Form.Field>
+              <div className="propertyNameBtnSubmit">
+                <Button type="submit">Save</Button>{" "}
+              </div>
+            </Form>
+          </Container>
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default GuestEditor;
